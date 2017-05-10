7ae2e105b8164f39805141eba61c80ab536704da
Sumukha K V <kvsumukha@gmail.com>
2017-05-10 13:24:02 +0530

    Add files via upload

--- src/thefirstone/MainGUI.java
+++ src/thefirstone/MainGUI.java
@@ -15,18 +15,7 @@ public class MainGUI {
 				FlowLayout fLayout = new FlowLayout(FlowLayout.LEFT, 5, 30);
 				frame2.setLayout(fLayout);
 				JLabel fileExtLabel = new JLabel("Select a File");
-				JButton button1 = new JButton("Open");
-				button1.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						JFileChooser filePath = new JFileChooser();
-						int returnVal = filePath.showOpenDialog(button1);
-						if(e.getSource() == button1) {
-							if (returnVal == JFileChooser.APPROVE_OPTION) {
-								File file = filePath.getSelectedFile();
-							}
-						}
-					}
-				});
+				JButton button1 = new JButton("Open");		
 				JTextField fileExt = new JTextField(20);
 				JLabel passwLabel = new JLabel("Enter the Password");
 				JTextField passwText = new JTextField(10);
@@ -36,6 +25,34 @@ public class MainGUI {
 				JTextField destText = new JTextField(10);
 				JLabel portLabel = new JLabel("Enter the Port");
 				JTextField portText = new JTextField(5);
+				JButton button2 = new JButton("Send");
+				/* The details of the objects used
+				 * frame 2 - It is the object for 2nd frame which starts upon clicking the "Send" button in frame1
+				 * fileExtLabel - For writing the Label field "Select a File"
+				 * button1 - For Open button
+				 * fileExt - Text field for receiving the path of the file to be sent
+				 * passwLabel - For writing the label field "Enter the Password"
+				 * passwText - Text field for receiving the Password
+				 * passlLabel - For writing the label field "Set Password Length"
+				 * passlText - Text field for receiving the password length
+				 * destLabel - For writing the label field "Enter the destination address"
+				 * destText - Text field for receiving the Destination address
+				 * portLabel - For writing the label field "Enter the port"
+				 * portText - Text field for receiving the port number
+				 */
+				button1.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JFileChooser filePath = new JFileChooser();
+						filePath.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+						int returnVal = filePath.showOpenDialog(button1);
+						if(e.getSource() == button1) {
+							if (returnVal == JFileChooser.APPROVE_OPTION) {
+						        String filePathString = filePath.getSelectedFile().getAbsolutePath();
+						        fileExt.setText(filePathString); // Writes the file path into the fileExt Text field
+							}
+						}
+					}
+				});
 				frame2.add(fileExtLabel);
 				frame2.add(button1);
 				frame2.add(fileExt);
@@ -47,8 +64,6 @@ public class MainGUI {
 				frame2.add(destText);
 				frame2.add(portLabel);
 				frame2.add(portText);
-				
-				JButton button2 = new JButton("Send");
 				frame2.add(button2);
 				
 				frame2.setSize(500, 280);
